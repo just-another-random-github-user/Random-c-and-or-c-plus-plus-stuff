1a4185710edc79c210baa6e6c65d4f0edf7ea431
just-another-random-github-user <69769779+just-another-random-github-user@users.noreply.github.com>
2023-04-26 05:22:15 +0000

    Create forwarder.c

---
+++ forwarder/forwarder.c
@@ -0,0 +1,13 @@
+//Yes, this is the only name I can think of for this.
+//It forwards any calls to it to a compile time declared path.
+#include <unistd.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+	#ifndef PATH
+	#error Please add -DPATH="path/to/executable"
+	#else
+	argv[0]=(char*)"#PATH";
+	execve(argv[0],argv,envp);
+	#endif
+	return 1;
+}
