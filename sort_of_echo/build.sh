1cef6de53dceca80e702163ed59b4d0e7c9f0675
just-another-random-github-user <69769779+just-another-random-github-user@users.noreply.github.com>
2023-05-05 23:38:44 +0000

    Update build.sh

--- sort_of_echo/build.sh
+++ sort_of_echo/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-gcc -O0 -s -o ./sizecheck ./main.c
-SIZE="$(stat --printf="%s" ./sizecheck)"
-rm ./sizecheck
-gcc -O3 -s -o ./out ./main.c "-DFILE_SIZE=$SIZE"
+gcc -O0 -s -o sizecheck main.c -DFILE_SIZE=0
+if [ "$?" = "1" ]; then return 1; fi
+SIZE="$(stat --printf="%s" sizecheck)"
+rm sizecheck
+gcc -O3 -s -o out main.c "-DFILE_SIZE=$SIZE"
