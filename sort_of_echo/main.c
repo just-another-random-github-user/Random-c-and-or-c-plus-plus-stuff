98d2cf93f6d1739888b0e1e891ac1749d3cd1a8c
just-another-random-github-user <69769779+just-another-random-github-user@users.noreply.github.com>
2023-05-05 23:38:31 +0000

    Update main.c

--- sort_of_echo/main.c
+++ sort_of_echo/main.c
@@ -2,8 +2,10 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #ifndef FILE_SIZE
+#error File size undefined.
 #define FILE_SIZE 0
 #endif
 
@@ -13,14 +15,23 @@ int main(int argc, char** argv, char** envp) {
 	int64_t off = (int64_t)lseek(fd, FILE_SIZE, SEEK_SET);
 	char* ptr = (char*) malloc(end - off + 1);
 	int64_t readL = (int64_t) read(fd, (void*) ptr, end - off);
-	if (readL == 0) { //If there's no data
+	if (readL == 0) { //No data
 		printf("No data has been appended. Append a string and run again.\n");
 		return 1;
 	}
-	if (readL == -1) { //If the read length is negative (and/or an error occurs)
+	if (readL == -1) { //Error
 		printf("Error.\n");
 		return 1;
 	}
-	printf("%.*s", (int) readL, ptr); //Print string truncated to the length read
+	ssize_t writeL = 0;
+	ssize_t temp = 0;
+	while (writeL < readL) { //So as to retry if EINTR or reaches write limit
+		temp = write(1,ptr+writeL,readL-writeL);
+		if (temp == -1) {
+			if (errno == EINTR) continue;
+			return 1; //Not much else you can do
+		}
+		writeL += temp;
+	}
 	return 0;
 }
